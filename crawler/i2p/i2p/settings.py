860d29a9f32ce2515be3c64238c6c97706a8b785
Alberto Abellán <albertoabellan.jhs@gmail.com>
2018-12-02 22:24:01 +0100

    Setting the maximum depth that will be allowed to crawl for any site

--- crawler/i2p/i2p/settings.py
+++ crawler/i2p/i2p/settings.py
@@ -70,18 +70,8 @@ DOWNLOADER_MIDDLEWARES = {
 #    'i2p.pipelines.I2PPipeline': 300,
 #}
 
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://doc.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+# The maximum depth that will be allowed to crawl for any site:
+DEPTH_LIMIT = 3
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings

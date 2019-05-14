78280b659a276f6344c33d2a868fb4a3a004ceaf
roberto <robertomagan@gmail.com>
2019-05-14 10:45:28 +0200

    Minor changes on deployment/management scripts.

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -3,10 +3,10 @@
 # Stops the crawling process gracefully
 # Author: Roberto Magan, 2019
 
-echo "Killing manager with PID=`cat pid.txt`"
-
-# stopping the manager (hard stop)
+echo "[+] Stopping crawling proces with UUID=`cat uuid.txt`"
+echo "--> Killing manager with PID=`cat pid.txt`"
+# stopping the manager (hard stop)`cat pid.txt`
 kill -9 `cat pid.txt`
-
+echo "--> Killing spiders ..."
 # stopping spiders (hard stop)
 kill -9 `pgrep scrapy`

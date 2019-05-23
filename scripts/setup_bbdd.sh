d892778c44246e4feed0fdb6e5fcfc2fafc47247
roberto <robertomagan@gmail.com>
2019-05-23 13:59:31 +0200

    Adding root_path to the setup_bbdd.sh script

--- scripts/setup_bbdd.sh
+++ scripts/setup_bbdd.sh
@@ -3,13 +3,15 @@
 # Resetting database and schema
 # Author: Roberto Magan, 2019
 
+root_path=~/RMAGAN/projects/I2P_Crawler
+
 echo "--> Deleting/creating database"
 mysql -u i2p -p4=XoG\!\*L -e "drop database i2p_database; create database i2p_database"
 
 echo "--> Populate database"
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate py27
-python crawler/database/populate.py
+python $root_path/crawler/database/populate.py
 conda deactivate
 
 

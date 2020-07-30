d8871e572733d48291690d6a621c392ef8524153
Emilio Figueras <emilio.figuerasmartin@alum.uca.es>
2020-07-30 20:38:05 +0200

    Update freenet scripts

--- scripts/freenet/setup_bbdd.sh
+++ scripts/freenet/setup_bbdd.sh
@@ -3,12 +3,8 @@
 # Resetting database and schema
 # Author: Emilio Figueras
 
-root_path=~/RMAGAN/projects/I2P_Crawler
+root_path=~/RMAGAN/projects/I2P_Crawler/crawler
 cd $root_path
-
-echo "[+] Creating folders..."
-mkdir -p logs
-echo " "
 
 echo "--> Deleting/creating database"
 mysql -u freenet -p1Uchn53d -e "drop database freenet_database; create database freenet_database"
@@ -16,7 +12,7 @@ mysql -u freenet -p1Uchn53d -e "drop database freenet_database; create database 
 echo "--> Populate database"
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate py37
-python crawler/populate.py
+python populate.py
 conda deactivate
 
 

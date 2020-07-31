20dae9a84718c7b5ec3b57b5a3296de4deba2400
Emilio Figueras <emilio.figuerasmartin@alum.uca.es>
2020-07-31 12:28:09 +0200

    Update freenet scripts

--- scripts/freenet/clean.sh
+++ scripts/freenet/clean.sh
@@ -3,13 +3,10 @@
 # Deleting files
 # Author: Emilio Figueras, 2020
 
-root_path=~/RMAGAN/projects/I2P_Crawler
-
-cd $root_path
 
 echo "[+] Deleting log and output files"
 echo "--> Deleting log output"
-find logs -type f -delete
+find /home/administrador/datos/freenet/logs -type f -delete
 echo "--> Deleting spiders output"
-find crawler/i2p/spiders/ongoing -type f -delete
-find crawler/i2p/spiders/finished -type f -delete
+find /home/administrador/datos/freenet/spiders/ongoing -type f -delete
+find /home/administrador/datos/freenet/spiders/finished -type f -delete
--- scripts/freenet/setup.sh
+++ scripts/freenet/setup.sh
@@ -4,12 +4,12 @@
 # Author: Emilio Figueras, 2020
 
 root_path=~/RMAGAN/projects/I2P_Crawler
-cd $root_path
 
 echo "[+] Creating folders..."
-mkdir -p logs
-mkdir -p crawler/i2p/spiders/ongoing
-mkdir -p crawler/i2p/spiders/finished
+mkdir -p /home/administrador/datos/freenet/logs
+mkdir -p /home/administrador/datos/freenet/spiders
+mkdir -p /home/administrador/datos/freenet/spiders/ongoing
+mkdir -p /home/administrador/datos/freenet/spiders/finished
 echo " "
 
 cd $root_path/crawler
@@ -22,7 +22,7 @@ echo " "
 
 echo "[+] Deleting log and output files"
 echo "--> Deleting log output"
-find logs -type f -delete
+find /home/administrador/datos/freenet/logs -type f -delete
 echo "--> Deleting spiders output"
-find crawler/i2p/spiders/ongoing -type f -delete
-find crawler/i2p/spiders/finished -type f -delete
+find /home/administrador/datos/freenet/spiders/ongoing -type f -delete
+find /home/administrador/datos/freenet/spiders/finished -type f -delete

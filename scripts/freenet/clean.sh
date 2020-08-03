f70080c7d7400c4bd894b81e3fbf7da032675dc0
Emilio Figueras <emilio.figuerasmartin@alum.uca.es>
2020-08-03 20:18:40 +0200

    Update freenet scripts

--- scripts/freenet/clean.sh
+++ scripts/freenet/clean.sh
@@ -10,3 +10,5 @@ find /home/administrador/datos/freenet/logs -type f -delete
 echo "--> Deleting spiders output"
 find /home/administrador/datos/freenet/spiders/ongoing -type f -delete
 find /home/administrador/datos/freenet/spiders/finished -type f -delete
+echo "--> Deleting uuid.txt"
+rm -f /home/administrador/RMAGAN/projects/I2P_Crawler/crawler/uuid.txt
